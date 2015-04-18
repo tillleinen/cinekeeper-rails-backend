8f38455d96e4f69711f28b6dc9693be06754d6ae
Till Leinen <hello@till.is>
2015-04-18 12:17:00 +0200

    use AWS in production

---
+++ config/initializers/carrier_wave.rb
@@ -0,0 +1,14 @@
+CarrierWave.configure do |config|
+  if Rails.env.production?
+    config.storage = :fog
+    config.fog_credentials = {
+      :provider               => 'AWS',
+      :aws_access_key_id      => ENV['AWS_KEY'],
+      :aws_secret_access_key  => ENV['AWS_SECRET'],
+      :region                 => ENV['AWS_REGION']
+    }
+    config.fog_directory  = ENV['AWS_BUCKET'] 
+  else 
+    config.storage = :file
+  end
+end
